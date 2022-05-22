04f322a8a23e95f5dbb61ae444a35b1ac27467dd
junghun95 <hun950803@naver.com>
2022-05-23 02:15:42 +0900

    video-autoplay

--- ssafit/src/components/video/VideoList.vue
+++ ssafit/src/components/video/VideoList.vue
@@ -9,7 +9,7 @@
             <iframe
             width="320"
             height="180"
-            :src="`${video.thumbnail}`"
+            :src="`https://www.youtube.com/embed/${video.id}`"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
